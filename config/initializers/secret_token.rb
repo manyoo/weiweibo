2d1ce30539d3c6a8ab625d4c0579d43c8069fdc2
Eric Wong <ericsyw@gmail.com>
2012-10-07 14:24:58 +0800

    Initial commit. Setup gem requirements and use RSpec instead of default test framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weiweibo::Application.config.secret_token = 'cfa8eb05f09deef19dd44308eadc9c97142f7d331714a8b0d9d29d841f64c1feb172615ea94344bf6e448eabab9fe20fd690c29a20f8eefb248e55e1fbe426cd'
