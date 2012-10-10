c4dfcfd9cad089de29a7552509ed6a99e9b2e168
Eric Wong <ericsyw@gmail.com>
2012-10-10 20:42:24 +0800

    Add pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
     factory :user do
-        name        "Eric Wong"
-        email       "ericsyw@gmail.com"
+        sequence(:name) { |n| "Person #{n}" }
+        sequence(:email) { |n| "person_#{n}@example.com" }
         password    "123456"
         password_confirmation "123456"
     end
